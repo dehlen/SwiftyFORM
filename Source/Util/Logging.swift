4145488b86b541a610b1ffdda6d2d44dce047074
Ehlen, David <david.ehlen@dm.de>
2020-06-12 16:47:44 +0200

    disable logging via build argument

--- Source/Util/Logging.swift
+++ Source/Util/Logging.swift
@@ -4,12 +4,10 @@ import Foundation
 /// `Print` with additional info such as linenumber, filename
 ///
 /// http://stackoverflow.com/questions/24114288/macros-in-swift
-#if DEBUG
-	func SwiftyFormLog(_ message: String, function: String = #function, file: String = #file, line: Int = #line) {
-		print("[\(file):\(line)] \(function) - \(message)")
-	}
-#else
-	func SwiftyFormLog(_ message: String, function: String = #function, file: String = #file, line: Int = #line) {
-		// do nothing
-	}
-#endif
+
+func SwiftyFormLog(_ message: String, function: String = #function, file: String = #file, line: Int = #line) {
+    if ProcessInfo.processInfo.arguments.contains("SwiftyFormLog") {
+        print("[\(file):\(line)] \(function) - \(message)")
+    }
+}
+
